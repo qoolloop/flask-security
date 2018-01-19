7c7221ce5ff56591ca22b10625acc5485f80e28e
Kan Torii <kan@qoolloop.com>
2018-01-19 18:34:18 +0900

    Added SECURITY_POST_FORGOT_VIEW for configuration. This specifies the page to transition to after sending instructions to reset password. Beware that the behavior has changed to transition to the login page, if the value is not set.

--- flask_security/core.py
+++ flask_security/core.py
@@ -69,6 +69,7 @@ _default_config = {
     'POST_LOGOUT_VIEW': '/',
     'CONFIRM_ERROR_VIEW': None,
     'POST_REGISTER_VIEW': None,
+    'POST_FORGOT_VIEW': None,
     'POST_CONFIRM_VIEW': None,
     'POST_RESET_VIEW': None,
     'POST_CHANGE_VIEW': None,
